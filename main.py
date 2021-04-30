028b2ff434245d8303f438a5e6e8f8bc66ab8c0b
thiago-esteves <testeves.mao@gmail.com>
2021-04-29 20:17:47 -0400

    enviar arq

---
+++ main.py
@@ -0,0 +1,9 @@
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        num1 = int(input("digite um numero: "))
+        num2 = int(input("digite um numero: "))
+
+        print(num1, num2)
+
+
